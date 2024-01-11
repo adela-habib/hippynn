21885fb78613d0cacf4d7c406d413061da5993ac
Adela Habib <ahabib@pn2035322.lanl.gov>
2024-01-11 14:32:25 -0700

    Enable interfacing part

--- hippynn/layers/physics.py
+++ hippynn/layers/physics.py
@@ -343,9 +343,9 @@ class ZBLPotential(torch.nn.Module):
                                 option_2,
                                 )
                              )
-        #atom_output = torch.zeros(species.shape, device=pair_output.device, dtype=pair_output.dtype)
-        #atom_output.index_add_(0, pair_first, pair_output)
-        return pair_output# , atom_output
+        atom_output = torch.zeros(species.shape, device=pair_output.device, dtype=pair_output.dtype)
+        atom_output.index_add_(0, pair_first, pair_output)
+        return pair_output , atom_output
        # output
        #  if r > self.r_outer:
        #      return 0, 0
