dae797741464b600b85a5fd95bee1f01e87a6fc2
Adela Habib <ahabib@pn2035322.lanl.gov>
2024-01-11 14:42:10 -0700

    Fixing shape in atom energies of the ZBL

--- hippynn/layers/physics.py
+++ hippynn/layers/physics.py
@@ -343,7 +343,7 @@ class ZBLPotential(torch.nn.Module):
                                 option_2,
                                 )
                              )
-        atom_output = torch.zeros(species.shape, device=pair_output.device, dtype=pair_output.dtype)
+        atom_output = torch.zeros(species.shape[0], device=pair_output.device, dtype=pair_output.dtype)
         atom_output.index_add_(0, pair_first, pair_output)
         return pair_output , atom_output
        # output
